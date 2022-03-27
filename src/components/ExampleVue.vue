28bb1301dcc230367f3dec2f0419e0a294729e8f
ashish <ashish.gupta@squareboat.com>
2022-03-27 16:47:24 +0530

    CHORE: start of the project

---
+++ src/components/ExampleVue.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="shadow-lg m-2 px-4 py-6">
+
+    <h3 class="font-bold text-2xl text-red-400">Title: {{title}}</h3>
+    <h2 class="text-blue-400">My name is {{getName('Ashish K')}}</h2>
+    <h2 class="text-green-400">My employer is {{getName('Gaurav Gupta')}}</h2>
+    <h3 class=""> name: {{getData().name}} and email: {{getData().email}}</h3>
+    <h3>My email is {{email}}</h3>
+    <h4>my phone is {{phone}}</h4>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'ExampleVue',
+    props: {
+        title: String
+    },
+    data() {
+        return {
+            name: 'Ashish Gupta',
+            email: 'Ashish@abc.com',
+            phone: 10203030,
+        }
+    },
+    methods: {
+        getName(x) {
+            return x
+        },
+        getData() {
+            return {
+                name: this.name,
+                email: this.email
+            }
+        }
+    },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="flex justify-center items-center flex-wrap">
+        <ExampleVue title="Love you jindagi" />
+        <ExampleVue title="Maje karo" />
+        <ExampleVue title="Had kardi aapne" />
+        <ExampleVue title="kya kar rahi ho bhai" />
+    </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import ExampleVue from '@/components/ExampleVue.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'HomeView',
+    components: {
+        HelloWorld,
+        ExampleVue,
+    }
 }
 </script>
